89a8dadd81c4e1951829aeed5034c3a1a16e5f91
Gabriel Cabral <cabral.9santos@gmail.com>
2019-02-04 22:41:46 -0200

    Primeiros componentes

--- App/components/CardPacote.js
+++ App/components/CardPacote.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View } from 'react-native';
 
 export default class CardPacote extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Text>JSON.stringify(this.props.detalhes, undefined, 2)</Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- App/views/ListaPacotes.js
+++ App/views/ListaPacotes.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View } from 'react-native';
+import CardPacote from '../components/CardPacote';
 
 export default class ListaPacotes extends React.Component {
+  state = {
+    pacotes: undefined
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Text>Hello World!</Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
