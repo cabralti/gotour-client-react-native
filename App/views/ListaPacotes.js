3ae2a78cf76fe50bfe103753f999862600184a97
Gabriel Cabral <cabral.9santos@gmail.com>
2019-02-04 22:41:46 -0200

    Listando os pacotes de viagem

--- App/views/ListaPacotes.js
+++ App/views/ListaPacotes.js
@@ -1,21 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, Alert } from 'react-native';
+import CardPacote from '../components/CardPacote';
 
 export default class ListaPacotes extends React.Component {
+  state = {
+    pacotes: undefined
+  }
+
+  componentDidMount(){
+    fetch('http://devup.com.br/gotour/api/pacotes')
+    .then(T => {
+
+      if(T.status === 405){
+        throw new Error(T.statusText);
+      }
+
+      return T.json()
+    })
+    .then(pacotes => this.setState({pacotes}))
+    .catch(error => {
+        if(error.message === '405'){
+          Alert.alert('Erro', 'Erro interno da api')
+        }else{
+          Alert.alert('Erro', 'Não foi possível recuperar os pacotes')
+        }
+    })
+  }
+
   render() {
+
+    const { pacotes } =   this.state;
+
+    if(!pacotes){
+        return (
+          <View />
+        );
+    }
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        {pacotes.map((pacote, key) => (
+            <CardPacote detalhes={pacote} key={key}/>
+        ))}
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
