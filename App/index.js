e769e177144169d9644db7e0e1bc7f9a73f3cfa1
Gabriel Cabral <cabral.9santos@gmail.com>
2019-02-05 22:29:11 -0200

    Criação do detalhe

--- App/index.js
+++ App/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ListaPacotes from './views/ListaPacotes';
+import DetalhePacote from './views/DetalhePacote';
 
 export default class App extends React.Component {
   render() {
     return (
-      <ListaPacotes/>
+      <DetalhePacote />
     );
   }
 }
--- App/views/DetalhePacote.js
+++ App/views/DetalhePacote.js
@@ -1,21 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Alert, ImageBackground, Button } from 'react-native';
 
 export default class DetalhePacote extends React.Component {
+  
+  state = {
+    dados : undefined
+  }
+
+  //recuperando id do pacote do servidor
+  componentDidMount(){
+    fetch('http://devup.com.br/gotour/api/pacote/88/detalhes')
+    .then(T => T.json())
+    .then(dados => this.setState({ dados }))
+    .catch(() => Alert.alert('Erro', 'Não foi possível recuperar os detalhes do pacote'))
+  }
+
   render() {
+    
+    const { dados } = this.state
+
+    if(!dados){
+        return (
+          <View />
+        )
+    }
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{paddingTop: 30}}>
+      <ImageBackground
+      resizeMode='cover'
+      style={{witdh:'100%', height: 200}}
+      source={{uri: dados.urlImagem}}>
+
+      <Text>{dados.pacote.nome.toUpperCase()}</Text>
+      </ImageBackground>
+        <View>
+          <Text>Descrição: {dados.descricao}</Text>
+          <Text>Telefone: {dados.telefone}</Text>
+          <Text>URL: {dados.site}</Text>
+          <Text>
+            Preço: {dados.pacote.valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'} )}
+          </Text>
+        </View>
+        <Button title='Voltar' onPress={() => console.log('voltar')}></Button>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
